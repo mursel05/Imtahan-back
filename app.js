9dc40423d55be4ac5444809f99de1bbee2ed9751
Mursel <mursal.haxverdiyev@gmail.com>
2025-03-19 23:33:52 +0400

    cors fixed

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const port = process.env.PORT;
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://128.203.100.101"],
+    origin: ["http://localhost:3000", "https://135.234.243.134"],
     credentials: true,
   })
 );
